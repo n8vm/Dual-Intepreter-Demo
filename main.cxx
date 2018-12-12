059bbe6f0a528095430f6809923f9f306452a6b7
Nathan Morrical <natemorrical@gmail.com>
2018-12-11 18:51:50 -0700

    Refactored a bit

--- main.cxx
+++ main.cxx
@@ -1,3 +1,5 @@
+// Nate VM. Run two subinterpreters which share the same underlying "BridgeLib" memory space 
+// This is helpful when trying to connect a PyQt GUI with an interactive interpreter, like Jupyter
 #include <thread>
 #include <Python.h>
 
@@ -7,40 +9,38 @@ int main()
     PyEval_InitThreads();
 
     auto main_state = PyThreadState_Get();
-
-    auto s1_state = Py_NewInterpreter();
+    auto subinterpreter_state = Py_NewInterpreter();
     PyThreadState_Swap(main_state);
 
-    auto A = [] (PyInterpreterState *interp)
+    std::thread t([] (PyInterpreterState *interp)
     {
         PyEval_AcquireLock();
         auto thread_state = PyThreadState_New(interp);
         auto thread_state_swap = PyThreadState_Swap(thread_state);
         
-        // PyRun_SimpleString("for i in range(0, 1000): print('I am subinterpreter A')");
+        PyRun_SimpleString("import Bridge");
+        PyRun_SimpleString("Bridge.set_x(42)");
+        PyRun_SimpleString("print('Try calling Bridge.set_x(<value>) or Bridge.get_x()')");
         PyRun_InteractiveLoop(stdin, "<stdin>");
 
         PyThreadState_Swap(thread_state_swap);
         PyThreadState_Clear(thread_state);
         PyThreadState_Delete(thread_state);
         PyEval_ReleaseLock();
-    };
+    }, subinterpreter_state->interp);
 
-    std::thread t1(A, s1_state->interp);
+    #ifdef NDEBUG
+        PyRun_SimpleString("import QtGUI");
+    #else
+        std::cout<<"Error: PyQt5 doesnt seem to work in a debug build of python 3. "<<std::endl;
+    #endif
 
-    PyRun_SimpleString("import QtGUI");
+    t.join();
 
-    auto _state = PyEval_SaveThread();
+    auto subinterpreter_state_swap = PyThreadState_Swap(subinterpreter_state);
+    Py_EndInterpreter(subinterpreter_state);
+    PyThreadState_Swap(subinterpreter_state_swap);
 
-    t1.join();
-
-    std::getchar();
-
-    auto s1_state_swap = PyThreadState_Swap(s1_state);
-    Py_EndInterpreter(s1_state);
-    PyThreadState_Swap(s1_state_swap);
-
-    PyEval_RestoreThread(_state);
     Py_Finalize();
     return 0;
 }
